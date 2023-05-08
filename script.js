6dca9a273aa08216eaf98b3fd12ac50a74270550
bigchills01 <ezechidera90@gmail.com>
2023-05-08 14:10:19 +0100

    javascript

---
+++ script.js
@@ -0,0 +1,67 @@
+/*function test(){
+    
+}
+
+
+//write a function that sums 2 arguments
+function add(a,b){
+    sum = 0;
+    sum = a + b;
+    return sum;
+}
+
+//create a function that converts minutes into seconds
+function convert(minutes) {
+	let seconds=60;
+	sum = 0;
+	sum = seconds * minutes;
+	return sum;
+}
+
+// create a function that calculte the circuit power
+function circuitPower(voltage, current) {
+	sum = 0;
+	sum = voltage * current;
+	return sum;
+}
+
+//create a function that returns a remainder
+function remainder(x, y) {
+    modulo = 0;
+        modulo = x % y;
+        return modulo;
+    }
+
+// calculate the edge of a triangle
+    function nextEdge(side1, side2) {
+        Edge = 0;
+        Edge = side1 + side2 -1;
+        return Edge;
+    }
+     //create afunction that takes a number as an argument and incement it by +1
+    function addition(num) {
+	let sum=1;
+	return sum+=num;
+	
+}
+//write a function that calculte the sum of internal polygon
+function sumPolygon(n) {
+	let formular =(n-2)*180;
+	return formular;
+	
+}
+ //Create a function that takes the age in years and returns the age in days.
+function calcAge(age) {
+	let daysinyear =365;
+	let totaldaysinyear = daysinyear * age;
+	return totaldaysinyear;
+}
+*/ 
+
+function getfirstelement(arr){
+ for(i =0; i > arr.length; i++){
+    arr = arr.length -1 ;
+ }
+ return arr;
+}
+console.log(arr)
